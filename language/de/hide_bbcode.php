7adc57dcd6cf574c7104efe9b2ba22c875af9cb2
Marco Kubuntu <marcovoort@gmail.com>
2015-06-27 09:45:04 +0200

    Added german language

---
+++ language/de/hide_bbcode.php
@@ -0,0 +1,48 @@
+<?php
+/** 
+*
+* simple_hide_bbcode [German]
+*
+* @package language
+* @version $Id: simple_hide_bbcode.php,v 1.0 2007/08/04 ycl6 Exp $
+* @copyright Marco van Oort https://github.com/marcovo/ , Miri4ever
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License 
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+
+/**
+* NOTE: Most of the language items are used in javascript
+* If you want to use quotes or other chars that need escaped, be sure you escape them double 
+* (Especially for ', you must use \\\' instead of \'. For " you only need to use \".
+*/
+/**
+ * @TODO:Change this to uppercase things.
+ */ 
+$lang = array_merge($lang, array(
+	'HIDEBB_HIDDEN_MESSAGE'   =>   'Diese Nachricht wurde versteckt',
+	'HIDEBB_MESSAGE_UNHIDE'   =>   'Versteckte Nachricht: Aus',
+	'HIDEBB_MESSAGE_HIDDEN'   =>   'Versteckte Nachricht: An',
+	'HIDEBB_MESSAGE_HIDDEN_DESCRIPTION'   =>   'Sie müssen auf diesen Beitrag antworten um die versteckte Nachricht lesen zu können.',
+));
+
+?>
